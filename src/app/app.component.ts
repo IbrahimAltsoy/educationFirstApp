613c573a98de9460320dffff778f8594a991e418
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-12-08 01:02:11 +0300

    08.12.2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model } from './model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Eğitim için oluşturulmuş bir angular peojsidir. Başarılar';
+  title = 'Eğitim için oluşturulmuş bir angular projsidir. Başarılar';
   name= 'İbrahim';
+  model = new Model();
+  getName(){
+    return this.model.user;
+  }
+  getItem(){
+    return this.model.items.filter(item=>!item.action);
+  }
 }
---
+++ src/app/model.ts
@@ -0,0 +1,24 @@
+export class Model {
+  user;
+  items;
+  constructor() {
+    this.user = "ibrahim";
+    this.items = [
+      new TodoItem("Spor", false),
+      new TodoItem("Kahvaltı", true),
+      new TodoItem("Kod Yazma", true),
+      new TodoItem("İngilizce Speaking", false),
+
+    ];
+
+  }
+}
+// const m = new Model();
+export class TodoItem {
+  description;
+  action;
+  constructor(description:string, action:boolean ){
+    this.description = description;
+    this.action = action;
+  }
+}
